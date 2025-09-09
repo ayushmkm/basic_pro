34aa4d3a88b12e1be5dbf500427708386285602a
Ayush Raj <ayushraj22201@gmail.com>
2025-09-09 10:55:02 +0530

    Updated the code with  inor changes

--- ayush.java
+++ ayush.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
-public class Name{
+public class ayush{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter your name : ");
     String name = sc.nextLine();
     System.out.println("Hello "+ name +" !");
+    sc.close();
   }
 }
