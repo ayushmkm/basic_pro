3dce3adf1ed437411b42f69250b46384ca2874cf
Ayush Raj <ayushraj22201@gmail.com>
2025-09-10 20:47:36 +0530

    2nd java pgrm

---
+++ calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class calculator{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter First number : ");
+        double number1 = scanner.nextDouble();
+        scanner.nextLine();
+        
+        System.out.println("Enter First number : ");
+        double number2 = scanner.nextDouble();
+        scanner.nextLine();
+        
+        System.out.println("What operation u want to perfrom (Sum,Subtraction,Multiply,Division : ");
+        String operation = scanner.nextLine();
+        
+        if(operation.equals("Sum")){
+            System.out.printf("%f + %f = %f",number1,number2,number1+number2);
+        }
+       if(operation.equals("Subtract")){
+            System.out.printf("%f - %f = %f",number1,number2,number1-number2);
+        }
+        if(operation.equals("Multiply")){
+            System.out.printf("%f * %f = %f",number1,number2,number1*number2);
+        }
+        if(operation.equals("Division")){
+            System.out.printf("%f / %f = %f",number1,number2,number1/number2);
+        }
+        scanner.close();
+    }
+}
